a9ddc6a9278aa6bea5bf7bdf01cb136d1b9c20c7
foxching <rechielagria@gmail.com>
2021-06-19 16:42:58 +0800

    fixed image

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -4,7 +4,7 @@ import classes from "./MeetupDetail.module.css";
 function MeetupDetail(props) {
   return (
     <section className={classes.detail}>
-      <Image src={props.image} width={600} height={500} />
+      <Image src={props.image} width={600} height={400} />
       <h1>{props.title}</h1>
       <address>{props.address}</address>
       <p>{props.description}</p>
